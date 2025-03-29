8b413fa81b13c678a2d7a47309259d948907fe68
BryanChanona <233299@ids.upchiapas.edu.mx>
2025-03-29 02:17:59 -0600

    Modificando main

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	temperatureDependencies "github.com/BryanChanona/backend_multi/src/Temperature/infrastructure/dependencies"
-	userDependencies "github.com/BryanChanona/backend_multi/src/User/infrastructure/dependencies"
-	routesUser "github.com/BryanChanona/backend_multi/src/User/infrastructure/routes"
 	"github.com/BryanChanona/backend_multi/src/helpers"
 	"github.com/gin-gonic/gin"
 	routesTemperature "github.com/BryanChanona/backend_multi/src/Temperature/infrastructure/routes"
@@ -14,7 +12,6 @@ import (
 )
 
 func main() {
-	userDependencies.Init()
 	temperatureDependencies.Init()
 	oxygenDependencies.Init()
 	heartRateDependencies.Init()
@@ -22,7 +19,6 @@ func main() {
 	 
 	r:= gin.Default()
 	helpers.InitCORS(r)
-	routesUser.Routes(r)
 	routesTemperature.Routes(r)
 	routesOxygen.Routes(r)
 	routesHeartRate.Routes(r)
